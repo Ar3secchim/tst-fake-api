14c9182965314a6c643b1e6a40b02b4b8501f6a4
Renara Secchim <renarasecchim@gmail.com>
2022-09-24 17:47:39 -0300

    feat: api

--- main.js
+++ main.js
@@ -1,8 +1,4 @@
-const perfil = document.querySelector('ul').innerText
-
-const containerComment = document.querySelector('#containerComment').innerText
-
-
+// capture data
 async function getData(){
   try{
     const promise = await fetch('https://jsonplaceholder.typicode.com/posts')
@@ -13,22 +9,57 @@ async function getData(){
     console.log('error')
   }
 }
+
+// create cotainer comment no html
 function createComment (element){
-  let userName = document.querySelector('#userName').innerText
-  let idUser = element.userId
+  // create container
+  const body = document.querySelector("body")
+  const containerBox = document.createElement("section")
+  containerBox.classList.add("container-box")
+  body.appendChild(containerBox)
 
-  userName.innerHTML = idUser
+  // create user
+  const userId = document.createElement("div")
+  userId.classList.add("user-id")
+  containerBox.appendChild(userId)
 
-  console.log(idUser)
+  userId.innerText = `USER ${element.userId}`
+
+  // create imgUser
+  const imgUser = document.createElement("img")
+  imgUser.src = ""
+  userId.appendChild(imgUser)
+
+  // create userName
+  const containerUserComment = document.createElement("div")
+  const userName = document.createElement("span")
+  userName.classList.add("user-name")
+  containerBox.appendChild(containerUserComment)
+
+  userName.innerText = element.id
+
+  //create container comment
+  containerUserComment.appendChild(userName)
+  containerUserComment.classList.add("containerUserComment")
+
+  const containerComment = document.createElement("span")
+  containerUserComment.appendChild(containerComment)
+  containerComment.classList.add("container-comment")
+
+  containerComment.innerText = element.body
+
+
 }
 
-
+// using dados
 async function main() {
     const data = await getData()
     const userName1 = await data.filter(data => data.userId === 1)
 
     userName1.forEach(element => {
       createComment(element)
+      console.log(element)
+
     })
 }
 
